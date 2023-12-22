aec8733ca2a9c4e51fbd1e597a692fd3b2e14ccf
Dandre Campbell <dandrecampbell5@gmail.com>
2023-12-22 10:01:19 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
 import Coin from './components/Coin';
 
 export default function App() {
-  const [coins, setCoins] = useState([]);
+    const [coins, setCoins] = useState([]);
 
-  useEffect(() => {
-    const intervals = setInterval(() => {
-      axios
-        .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-        .then(currency => {setCoins(currency.data);})
-        .catch(error => console.log(error));
-    }, 1000);
+    useEffect(() => {
+        const intervals = setInterval(() => {
+            axios
+                .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+                .then(currency => {setCoins(currency.data);})
+                .catch(error => console.log(error));
+        }, 1000);
 
     return () => clearInterval(intervals);
-  }, []);
+    }, []);
 
-  return (
-    <div className='text-white flex flex-col items-center mt-[65px] pb-[100px]'>
-    <h1 className='text-[42px] text-center mb-[32px]'>Cryptocurrency Market</h1>
-    <div className="flex flex-row justify-center items-center w-[100%] h-[75px]">
-      <div className="flex items-center pr-[24px] min-w-[250px]">
-        <h1 className="text-[16px] w-[150px] pl-[150px] crypto-name">Currency</h1>
-        <h1 className="text-[16px] pl-[150px]">Symbol</h1>
-      </div>
-      <div className="text-right flex justify-between w-[300px]">
-        <h1 className="flex justify-center w-[150px]">Stock Price</h1>
-      </div>
-    </div>
-      {coins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className='text-white flex flex-col items-center mt-[65px] pb-[100px]'>
+            <h1 className='text-[42px] text-center mb-[32px]'>Cryptocurrency Market</h1>
+            <div className="flex flex-row justify-center items-center w-[100%] h-[75px]">
+                <div className="flex items-center pr-[24px] min-w-[250px]">
+                    <h2 className="text-[16px] w-[150px] pl-[150px] crypto-name">Currency</h2>
+                    <h2 className="text-[16px] pl-[150px]">Symbol</h2>
+                </div>
+                <div className="text-right flex justify-between w-[300px]">
+                    <h2 className="flex justify-center w-[150px]">Stock Price</h2>
+                </div>
+            </div>
+            {coins.map(coin => {
+                return (
+                    <Coin
+                        key={coin.id}
+                        name={coin.name}
+                        price={coin.current_price}
+                        symbol={coin.symbol}
+                        marketcap={coin.total_volume}
+                        volume={coin.market_cap}
+                        image={coin.image}
+                        priceChange={coin.price_change_percentage_24h}
+                    />
+                );
+            })}
+        </div>
+    );
 }
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,30 +2,30 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Coin.css';
 
-const CoinTest = ({ name, price, symbol, image, priceChange }) => {
-  return (
-    <div>
-        <Container className="flex justify-center">
-            <div className='flex justify-center items-center'>
-                <Row className="flex flex-row justify-center items-center h-[75px] coin-row">
-                    <Col className='flex items-center min-w-[300px] pr-[24px]'>
-                        <img src={image} alt='crypto' className='w-[30px] h-[30px] mr-[15px]' />
-                        <h2 className='text-[16px] w-[150px]'>{name}</h2>
-                        <p className='uppercase coin-symbol'>{symbol}</p>
-                    </Col>
-                    <Col className='text-right flex justify-between w-[150px]'>
-                        <p className='w-[100px]'>${price}</p>
-                        {priceChange < 0 ? (
-                            <p className='text-[#f00606] w-[10px]'>{priceChange.toFixed(2)}%</p>
-                        ) : (
-                            <p className='text-[#11d811] w-[10px]'>{priceChange.toFixed(2)}%</p>
-                        )}
-                    </Col>               
-                </Row>
-            </div>
-        </Container>        
-    </div>
-  );
+const Coin = ({ name, price, symbol, image, priceChange }) => {
+    return (
+        <div>
+            <Container className="flex justify-center">
+                <div className='flex justify-center items-center'>
+                    <Row className="flex flex-row justify-center items-center h-[75px] coin-row">
+                        <Col className='flex items-center min-w-[300px] pr-[24px]'>
+                            <img src={image} alt='crypto' className='w-[30px] h-[30px] mr-[15px]' />
+                            <h2 className='text-[16px] w-[150px]'>{name}</h2>
+                            <p className='uppercase coin-symbol'>{symbol}</p>
+                        </Col>
+                        <Col className='text-right flex justify-between w-[150px]'>
+                            <p className='w-[100px]'>${price}</p>
+                            {priceChange < 0 ? (
+                                <p className='text-[#f00606] w-[10px]'>{priceChange.toFixed(2)}%</p>
+                            ) : (
+                                <p className='text-[#11d811] w-[10px]'>{priceChange.toFixed(2)}%</p>
+                            )}
+                        </Col>               
+                    </Row>
+                </div>
+            </Container>        
+        </div>
+    );
 }
 
-export default CoinTest;
+export default Coin;
